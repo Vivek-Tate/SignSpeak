15a2eed157095e490e7e05fbc65b2baf00f7cffe
Vivek Shravan Tate <vivektate.developer@gmail.com>
2025-02-23 20:51:00 +0000

    Initial Commit

---
+++ SignSpeak/SignSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignSpeakApp.swift
+//  SignSpeak
+//
+//  Created by Vivek Tate on 23-02-2025.
+//
+
+import SwiftUI
+
+@main
+struct SignSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
