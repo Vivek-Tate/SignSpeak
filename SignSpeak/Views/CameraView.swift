2ae5e88f0955af417dc871325af9d0455d6882ff
Vivek Shravan Tate <vivektate.developer@gmail.com>
2025-02-26 13:23:05 +0000

    Added Camera View.

---
+++ SignSpeak/Views/CameraView.swift
@@ -0,0 +1,64 @@
+//
+//  CameraView.swift
+//  SignSpeak
+//
+//  Created by Vivek Tate on 26-02-2025.
+//
+import SwiftUI
+
+struct CameraView: View {
+    
+    // MARK: Stored Properties
+    @StateObject private var cameraController = CameraViewController()
+
+    var body: some View {
+        ZStack {
+            
+            // Add CameraPreview to show live camera feed
+            CameraPreview(session: cameraController.session)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Spacer()
+
+                // Camera Control Panel
+                HStack {
+                    
+                    Button(action: { cameraController.toggleCameraFlash() }) {
+                        
+                        Image(systemName: cameraController.isFlashEnabled ? "bolt.fill" : "bolt.slash")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .padding()
+                    }
+
+                    Spacer()
+
+                    Button(action: { cameraController.startRecording() }) {
+                        Image(systemName: "record.circle")
+                            .foregroundStyle(.red)
+                            .font(.largeTitle)
+                            .padding()
+                    }
+
+                    Spacer()
+
+                    Button(action: { cameraController.switchCamera() }) {
+                        Image(systemName: "arrow.triangle.2.circlepath.camera")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .padding()
+                    }
+                }
+                .padding(.horizontal, 30)
+                .padding(.bottom, 50)
+            }
+        }
+        .onAppear {
+            cameraController.startCameraSession()
+        }
+        .onDisappear {
+            cameraController.stopCameraSession()
+        }
+    }
+}
